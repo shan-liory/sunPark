e6d0367ac5e89322dcfbd8458b0836a2bcb79122
shan-liory <92073344+shan-liory@users.noreply.github.com>
2023-05-30 14:24:55 +0800

    update screen in app

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -13,8 +13,12 @@ import java.util.ArrayList;
 
 // @react-native-community/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
+// react-native-camera
+import org.reactnative.camera.RNCameraPackage;
 // react-native-gesture-handler
 import com.swmansion.gesturehandler.RNGestureHandlerPackage;
+// react-native-permissions
+import com.zoontek.rnpermissions.RNPermissionsPackage;
 // react-native-reanimated
 import com.swmansion.reanimated.ReanimatedPackage;
 // react-native-safe-area-context
@@ -69,7 +73,9 @@ public class PackageList {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
       new RNCMaskedViewPackage(),
+      new RNCameraPackage(),
       new RNGestureHandlerPackage(),
+      new RNPermissionsPackage(),
       new ReanimatedPackage(),
       new SafeAreaContextPackage(),
       new RNScreensPackage(),
--- screen/ScanQR.tsx
+++ screen/ScanQR.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
-import { NativeBaseProvider, Box, Text } from 'native-base';
+import React, { useState } from 'react';
+import { NativeBaseProvider, Box, Text,Pressable,Center } from 'native-base';
+import axios from "axios"
+import { RNCamera } from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+// interface parkingLot {
+//   id: string;
+// }
+
+// const [parkingLot,setParkingLot] = useState<parkingLot>({
+//   id:''
+// })
+
+const onSuccess = async (e: string) => {
+  try{
+    await axios
+    .post("http://172.20.10.4:3500/parkingQrCode/get",
+      {parkingLot:e}
+    )
+    .then((res) => {
+      if (res.data.message === "success") {
+        console.log("scanned");
+      }
+    })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+  }
+  catch (err) {
+    console.error('An error occurred', err)
+  }
+};
+
 
 const ScanQR = () => {
   return (
     <NativeBaseProvider>
       <Box alignItems="center" bg="#003572" flex={1}>
+      {/* flashMode={RNCamera.Constants.FlashMode.torch} */}
         <Text fontSize="20" fontWeight="bold" mt={5} color="white">Scan QR Code</Text>
+        {/* <QRCodeScanner
+        onRead={onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        cameraStyle={{width:"90%", marginLeft:20, marginTop:5}}
+        topContent={
+          <Text style={{color:"white"}}>
+           Scan the provided parking QR code.
+          </Text>
+        }
+        bottomContent={
+         
+        }
+        /> */}
+         <Pressable onPress={() => onSuccess("647552b1c7f26f936ac45fad")}>
+          <Text>OK. Got it!</Text>
+           </Pressable>
       </Box>
     </NativeBaseProvider>
   );
